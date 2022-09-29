6cd2d5c611985ba62aea2401d574c074162afbb8
Ivan <ivanfergarcia@gmail.com>
2022-09-29 10:23:43 +0200

    Actualización 29/9/22

--- src/main/java/org/ejercicios/PingPong/Main.java
+++ src/main/java/org/ejercicios/PingPong/Main.java
@@ -6,13 +6,10 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
 
         Semaphore semaphore = new Semaphore(2);
-
         semaphore.acquire(1);
 
-        int times = 10;
-
-        Thread pingThread = new Thread(new PingThread(times, semaphore));
-        Thread pongThread = new Thread(new PongThread(times, semaphore));
+        Thread pingThread = new Thread(new PingThread(10, semaphore));
+        Thread pongThread = new Thread(new PongThread(10, semaphore));
 
         pingThread.start();
         pongThread.start();
